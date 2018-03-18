a66a2fb8dc76d3d1ae263940edea25d4f47ffb31
wangke <1052060838@qq.com>
2018-03-18 21:20:43 +0800

    合并mac上的代码，重新拉取

---
+++ app/src/main/java/com/merpyzf/xmshare/ui/test/recyclerview_demo/BaseAdapter/BaseAdapter.java
@@ -0,0 +1,12 @@
+package com.merpyzf.xmshare.ui.test.recyclerview_demo.BaseAdapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by wangke on 2018/2/9.
+ */
+
+public abstract class BaseAdapter<T, K extends BaseViewHolder> extends RecyclerView.Adapter<K> {
+
+
+}
---
+++ app/src/main/java/com/merpyzf/xmshare/ui/test/recyclerview_demo/TestRecyclerViewActivity.java
@@ -0,0 +1,15 @@
+package com.merpyzf.xmshare.ui.test.recyclerview_demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.merpyzf.xmshare.R;
+
+public class TestRecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_recycler_view);
+    }
+}
